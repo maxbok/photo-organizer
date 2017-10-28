b14a95312529ed117d744778a1409d50d64ad5b7
max <max@codehawks.eu>
2017-10-28 18:48:53 +0200

    Initial Commit

---
+++ PhotoOrganizer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PhotoOrganizer
+//
+//  Created by Maxime Bokobza on 28/10/2017.
+//  Copyright © 2017 Codehawks. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PhotoOrganizer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PhotoOrganizer
+//
+//  Created by Maxime Bokobza on 28/10/2017.
+//  Copyright © 2017 Codehawks. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
